39ca4cc0d7fedb65227112855f08ff38072896de
Ray Alez <raymestalez@gmail.com>
2015-04-21 19:36:52 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-// **************** Main ****************
-var spawn = require('spawn');
-var spawningUnit = 1;
-spawn(spawningUnit);
+// // **************** Main ****************
+// var spawn = require('spawn');
+// var spawningUnit = 1;
+// spawn(spawningUnit);
 
-var harvester = require('harvester');
-var builder = require('builder');
-var guard = require('guard');
+// var harvester = require('harvester');
+// var builder = require('builder');
+// var guard = require('guard');
 
-for(var name in Game.creeps) {
-    var creep = Game.creeps[name];
+// for(var name in Game.creeps) {
+//     var creep = Game.creeps[name];
 
-    if(creep.memory.role == 'harvester') {
-	harvester(creep);
-    }
+//     if(creep.memory.role == 'harvester') {
+// 	harvester(creep);
+//     }
 
-    if(creep.memory.role == 'builder') {
-	builder(creep);
-    }
+//     if(creep.memory.role == 'builder') {
+// 	builder(creep);
+//     }
 
-    if(creep.memory.role == 'guard') {
-	guard(creep);
-    }
-}
+//     if(creep.memory.role == 'guard') {
+// 	guard(creep);
+//     }
+// }
+// 
